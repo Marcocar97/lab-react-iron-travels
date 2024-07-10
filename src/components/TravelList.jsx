0d231e51880d9a4621ebfcaf2cdf3668b56ba8f8
Marcos <mmarkito708@gmail.com>
2024-07-10 19:54:16 +0200

    terminado

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -4,9 +4,14 @@ import travelPlansData from "../assets/travel-plans.json";
 function TravelList() {
   const [lugares, setLugares] = useState(travelPlansData);
 
+  const eliminar = (indexBorrar) => {
+    const clone = JSON.parse(JSON.stringify(lugares));
+    clone.splice(indexBorrar, 1);
+    setLugares(clone);
+  }
   return (
     <div>
-      {lugares.map((eachLugar) => {
+      {lugares.map((eachLugar, index) => {
         return (
           <div className="travel-card"> 
           <img src={eachLugar.image} alt="" />
@@ -20,6 +25,13 @@ function TravelList() {
             </h3>
             <p>{eachLugar.description}</p>
             <p>{eachLugar.totalCost} €</p>
+            {eachLugar.totalCost < 350 ? ( <span className="labelG">Gran Oferta</span> ) : null} 
+            {eachLugar.totalCost > 1500 ? ( <span className="label">Premium</span> ) : null}  
+            {eachLugar.allInclusive ? ( <> <span className="label">Todo Incluido</span> </> ) : null}  
+            <button onClick={() => eliminar(index)}>Eliminar</button>
+
+
+
           </div>
         );
       })}
